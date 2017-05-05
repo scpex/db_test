b1bdede2ab1550947436bece525674386fde2c01
S <mr.nathakit@live.com>
2017-05-05 15:35:37 +0700

    Connect sql server finish!!!
    insert update delete and save to database

--- db_test/Form1.Designer.cs
+++ db_test/Form1.Designer.cs
@@ -46,15 +46,16 @@
             // dataGV
             // 
             this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGV.Location = new System.Drawing.Point(13, 13);
+            this.dataGV.Location = new System.Drawing.Point(12, 12);
             this.dataGV.Name = "dataGV";
-            this.dataGV.Size = new System.Drawing.Size(553, 524);
+            this.dataGV.Size = new System.Drawing.Size(449, 440);
             this.dataGV.TabIndex = 0;
+            this.dataGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGV_CellContentClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 569);
+            this.label1.Location = new System.Drawing.Point(31, 483);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 1;
@@ -63,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 604);
+            this.label2.Location = new System.Drawing.Point(14, 518);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 2;
@@ -72,7 +73,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(66, 640);
+            this.label3.Location = new System.Drawing.Point(13, 554);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(36, 13);
             this.label3.TabIndex = 3;
@@ -82,7 +83,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(62, 675);
+            this.label4.Location = new System.Drawing.Point(9, 589);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 4;
@@ -90,21 +91,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 566);
+            this.textBox1.Location = new System.Drawing.Point(96, 480);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(332, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(149, 601);
+            this.textBox2.Location = new System.Drawing.Point(96, 515);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(332, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(149, 637);
+            this.textBox3.Location = new System.Drawing.Point(96, 551);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(332, 20);
             this.textBox3.TabIndex = 7;
@@ -112,43 +113,46 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(149, 672);
+            this.comboBox1.Location = new System.Drawing.Point(96, 586);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(332, 21);
             this.comboBox1.TabIndex = 8;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 727);
+            this.button1.Location = new System.Drawing.Point(59, 641);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 42);
             this.button1.TabIndex = 9;
             this.button1.Text = "Update";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(245, 727);
+            this.button2.Location = new System.Drawing.Point(194, 641);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 42);
             this.button2.TabIndex = 10;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(413, 727);
+            this.button3.Location = new System.Drawing.Point(327, 641);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(101, 42);
             this.button3.TabIndex = 11;
             this.button3.Text = "Save";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 812);
+            this.ClientSize = new System.Drawing.Size(477, 714);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
--- db_test/Form1.cs
+++ db_test/Form1.cs
@@ -18,19 +18,111 @@ namespace db_test
             InitializeComponent();
         }
 
-        DataSet ds = new DataSet();
-        string constr = "Data Source = .; Initial Catalog = dbtester; User ID = sa; Password = 12345";
+        DataSet ds = new DataSet(); // create data set
+        string constr = "Data Source = S-PC\\SQLEXPRESS; Initial Catalog = dbtester; User ID = sa; Password = 12345";   // create connection string
         private void Form1_Load(object sender, EventArgs e)
         {
+            // show all data on gridview
             string sql = "SELECT * FROM tb_Customer";
             SqlDataAdapter da = new SqlDataAdapter(sql, constr);
-            da.Fill(ds, "Customer");
-            dataGV.DataSource = ds.Tables["Customer"];
+            da.Fill(ds, "tb_Customer");
+            dataGV.DataSource = ds.Tables["tb_Customer"];
+
+            // show County_name data on combobox
+            sql = "SELECT * FROM tb_County";
+            da = new SqlDataAdapter(sql, constr);
+            da.Fill(ds, "tb_County");
+            comboBox1.DisplayMember = "County_name";        // เอาข้อมูลไปแสดงผล
+            comboBox1.ValueMember = "County_id";            // เอาข้อมูลไปแอบไว้
+            comboBox1.DataSource = ds.Tables["tb_County"];
         }
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void dataGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // select all datain row when you click each data in gridview and show data selected on textbox feil 
+
+            if (e.RowIndex == -1) { return; }           // เลือกอย่างอื่นที่ไม่ใช้ข้อมูล เช่น หัวของตาราง
+            dataGV.Rows[e.RowIndex].Selected = true;    // เมื่อกดตรงข้อมูล จะให้โชว์ข้อมูลทั้งแถว
+
+            //show data selected on textbox feil
+
+            //// sol 1 >> pull data from data table
+            //DataRow dr = ds.Tables["tb_Customer"].Rows[e.RowIndex];
+            //textBox1.Text = dr["Id"].ToString();
+            //textBox2.Text = dr["Name"].ToString();
+            //textBox3.Text = dr["Salary"].ToString();
+            //comboBox1.SelectedValue = dr["County_id"].ToString();
+
+            // sol 2 >> pull data from data gridview
+            DataGridViewRow dgvr = dataGV.Rows[e.RowIndex];
+            textBox1.Text = dgvr.Cells[0].Value.ToString();
+            textBox2.Text = dgvr.Cells[1].Value.ToString();
+            textBox3.Text = dgvr.Cells[2].Value.ToString();
+            comboBox1.SelectedValue = dgvr.Cells[3].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  Insert/Update data
+            DataRow[] drs = ds.Tables["tb_Customer"].Select("Id="+ textBox1.Text+"");
+            if(drs.Length == 0) 
+            {
+                // no have data (insert data)
+                DataRow dr = ds.Tables["tb_Customer"].NewRow(); // สร้างแถวใหม่
+                dr["Id"] = textBox1.Text;                       // ใส่ข้อมูลในแถวใหม่
+                dr["Name"] = textBox2.Text;
+                dr["Salary"] = textBox3.Text;
+                dr["County_id"] = comboBox1.SelectedValue;
+                ds.Tables["tb_Customer"].Rows.Add(dr);          // เอาข้อมูลในแถวใหม่ใส่ใน data table
+            }
+            else
+            {
+                // have data (update data)
+                drs[0]["Name"] = textBox2.Text;
+                drs[0]["Salary"] = textBox3.Text;
+                drs[0]["County_id"] = comboBox1.SelectedValue;
+            }
+
+            dataGV.DataSource = ds.Tables["tb_Customer"];   // update data grid view
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //  Delete data
+            DataRow[] drs = ds.Tables["tb_Customer"].Select("Id=" + textBox1.Text + "");
+            if(drs.Length == 0) 
+            {
+                // ไม่พบข้อมูลที่ต้องการลบ ให้แจ้งเตือน โดยใช้ message box
+                MessageBox.Show("Data not found!!!");
+            }
+            else
+            {
+                //  ถ้าพบข้อมูลที่ต้องการลบ ให้แจ้งเตือน โดยใช้ message box เพื่อยืนยันการลบข้อมูล แล้วทำการลบข้อมูลนั้น
+                drs[0].Delete();                            // ลบข้อมูลในแถว แต่แถวยังอยู่
+
+                //  ปรับปรุงฐานข้อมูลตรงนี้เลย เพื่อให้แถวของข้อมูลที่ลบไปนั้นกลับมาสู่สภาพปกติ
+                string sql = "SELECT * FROM tb_Customer";
+                SqlDataAdapter da = new SqlDataAdapter(sql, constr);
+                SqlCommandBuilder comb = new SqlCommandBuilder(da); // ใช้ปรับปรุงฐานข้อมูลที่มีการเปลี่ยนแปลง
+                da.Update(ds, "tb_Customer");
+
+                ds.Tables["tb_Customer"].AcceptChanges();   // จะทำให้แถวที่ว่างนั้น หายไป
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  Save to DB
+            string sql = "SELECT * FROM tb_Customer";
+            SqlDataAdapter da = new SqlDataAdapter(sql, constr);
+            SqlCommandBuilder comb = new SqlCommandBuilder(da); // ใช้ปรับปรุงฐานข้อมูลที่มีการเปลี่ยนแปลง
+            da.Update(ds, "tb_Customer");
+
+            MessageBox.Show("Data Saved into Database!!!");
+        }
     }
 }
