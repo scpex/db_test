0ce3899923557af6e0959e3ab82cd1d2c7f84539
S <mr.nathakit@live.com>
2017-05-22 16:18:38 +0700

    add winformapp

---
+++ eduDB/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace eduDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGV
+            // 
+            this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV.Location = new System.Drawing.Point(13, 13);
+            this.dataGV.Name = "dataGV";
+            this.dataGV.Size = new System.Drawing.Size(651, 259);
+            this.dataGV.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Finder";
+            // 
+            // tbx_search
+            // 
+            this.tbx_search.Location = new System.Drawing.Point(70, 280);
+            this.tbx_search.Name = "tbx_search";
+            this.tbx_search.Size = new System.Drawing.Size(443, 20);
+            this.tbx_search.TabIndex = 2;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(529, 278);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 3;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 348);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.tbx_search);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_search;
+        private System.Windows.Forms.Button btn_search;
+    }
+}
+
---
+++ eduDB/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eduDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ds = new DataSet(); // create data set
+        string constr = "Data Source = S-PC\\SQLEXPRESS; Initial Catalog = eduDB; User ID = sa; Password = 12345";   // create connection string
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // show all data on gridview
+            string sql = "SELECT Students.std_id, Students.std_name, Students.std_gender, Classes.cl_name, Teachers.tc_name, School.sc_name"+
+                         " FROM Students"+
+                         " INNER JOIN Classes ON Students.cl_id = Classes.cl_id"+
+                         " INNER JOIN Teachers ON Classes.tc_id = Teachers.tc_id"+
+                         " INNER JOIN School ON Teachers.sc_id = School.sc_id";
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, constr);
+            
+            da.Fill(dt);
+            dataGV.DataSource = dt;
+            
+        }
+    }
+}
